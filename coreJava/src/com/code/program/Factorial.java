17c3fa36998b9f310fa5bbbf3a35736910d472a5
Amit Naokar <naokar.amit@gmail.com>
2016-08-13 11:34:19 +0530

    factorial programm

---
+++ coreJava/src/com/code/program/Factorial.java
@@ -0,0 +1,28 @@
+package com.code.program;
+
+/**
+ * Created by Amit on 08/13/16.
+ */
+public class Factorial {
+
+    public static int calcFactorialNormal(int n){
+        int result = 1;
+        for(int i=n; i>0; i--){
+            result = result*i;
+        }
+        return result;
+    }
+
+    public static int calcFactorialUsingRecursion(int n){
+        if (n>0)
+            return n*calcFactorialNormal(n-1);
+        else
+            return 1;
+    }
+
+    public static void main(String args[]){
+        System.out.println(Factorial.calcFactorialNormal(5));
+        System.out.println(Factorial.calcFactorialUsingRecursion(5));
+    }
+
+}
